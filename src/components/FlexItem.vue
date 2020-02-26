a3c67a350c9dca4cdbfdbecbfa99f8e8c2c4e49c
Jonny <1043870470@qq.com>
2020-02-26 19:37:00 +0800

    [fix] 修复一些bug

--- src/components/FlexItem.vue
+++ src/components/FlexItem.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="c-flexbox-item" :style="[gutter, {flex: styleFlex}]">
+  <div class="c-flexbox-item" :style="[gutter, styleFlex]">
     <slot></slot>
   </div>
 </template>
 
 <script>
 function parseFlex(flex) {
-  if (typeof flex === "number") {
-    return `${flex} ${flex} auto`;
+  if (typeof flex === "number" || flex === '1') {
+    return {
+      flex: `${flex} ${flex} auto`,
+      width: 0
+    };
   }
 
   if (/^\d+(\.\d+)?(px|em|rem|vw|%)$/.test(flex)) {
-    return `0 0 ${flex}`;
+    return {flex: `0 0 ${flex}`};
   }
 
-  return flex;
+  return {flex};
 }
 export default {
   name: "FlexItem",
@@ -24,30 +27,22 @@ export default {
   computed: {
     gutter() {
       let parent = this.$parent;
-      console.log(1, parent);
-      while (parent && parent.$options.name !== "Flex") {
-        parent = parent.$parent;
-      }
-      console.log(2, parent);
+      if(parent && parent.$options.name !== "Flex") return {}
 
       if (!(parent && parent.gutter)) {
-        console.log(111);
         return {};
       } else if (typeof parent.gutter === "number") {
-        console.log(222);
         return parent.gutter > 0
           ? {
-              paddingLeft: `${this.gutter / 2}px`,
-              paddingRight: `${this.gutter / 2}px`
+              paddingLeft: `${parent.gutter / 2}px`,
+              paddingRight: `${parent.gutter / 2}px`
             }
           : {};
       } else if (typeof parent.gutter === "string") {
-        console.log(333);
         const [num, unit] = parent.gutter
           .replace(/(^\d+(\.\d+)?)(px|em|rem|vw|%)?/gi, "$1,$3")
           .split(",");
         const gutter = parseInt(num || "0", 10);
-        console.log(333, 'gutter', gutter, [num, unit]);
         return gutter > 0
           ? {
               paddingLeft: `${gutter / 2}${unit || 'px'}`,
@@ -55,11 +50,10 @@ export default {
             }
           : {};
       }
-      console.log(444);
       return {};
     },
     styleFlex() {
-      return this.flex ? parseFlex(this.flex) : undefined;
+      return this.flex ? parseFlex(this.flex) : {};
     }
   }
 };
